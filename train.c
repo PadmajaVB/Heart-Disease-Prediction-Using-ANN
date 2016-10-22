9dac99dad237929c6b27a5041ebbddbf09bb3cc7
PadmajaVB <padmajabhagwat4@gmail.com>
2016-10-22 22:58:45 +0530

    Code for training the model

--- train.c
+++ train.c
@@ -3,26 +3,92 @@
 #include <time.h>
 #include <math.h>
 #include <fcntl.h>
+#include <string.h>
 
-#define NUMPAT 4
-#define NUMIN  2
-#define NUMHID 2
-#define NUMOUT 1
+#define NUMPAT 20
+#define NUMIN  13
+#define NUMHID 13
+#define NUMOUT 3
 
 #define rando() ((double)rand()/(RAND_MAX)+1)
 
-int main() {
-    int    i, j, k, p, np, op, ranpat[NUMPAT+1], epoch;
-    int    NumPattern = NUMPAT, NumInput = NUMIN, NumHidden = NUMHID, NumOutput = NUMOUT;
-    double Input[NUMPAT+1][NUMIN+1] = { 0, 0, 0,  0, 0, 0,  0, 1, 0,  0, 0, 1,  0, 1, 1 };
-    double Target[NUMPAT+1][NUMOUT+1] = { 0, 0,  0, 0,  0, 1,  0, 1,  0, 0 };
+
+int main()
+{
+    int i, j, k, p, np, op, ranpat[NUMPAT+1], epoch;
+    int NumPattern = NUMPAT, NumInput = NUMIN, NumHidden = NUMHID, NumOutput = NUMOUT;
+    float Input[NUMPAT+1][NUMIN+1];
+    float Target[NUMPAT+1][NUMOUT+1];
+
+     //reading from csv files and storing it in 2D array
+     char buffer[1024] ;
+     char tbuffer[1024] ;
+     char *record,*line, *trecord, *tline;
+     int x=0,y=0,l=0,m=0;
+     //float mat[20][14];
+     FILE *fstream = fopen("cleveland.csv","r");
+     FILE *tstream = fopen("test.csv","r");
+
+     if(fstream == NULL || tstream == NULL)
+     {
+       printf("\n file opening failed ");
+       return -1 ;
+     }
+
+     while((line=fgets(buffer,sizeof(buffer),fstream))!=NULL && (tline=fgets(tbuffer,sizeof(tbuffer),tstream))!=NULL)
+     {
+       y=0;
+       m=0;
+       record = strtok(line,",");
+
+       while(record != NULL)
+       {
+        //here you can put the record into the array as per your requirement.
+        Input[x][y++] = atof(record) ;
+        record = strtok(NULL,",");
+//printf ("Input[%d][%d] = %f\n",x,y,Input[x][y]);
+       }
+       trecord = strtok(tline,",");
+       while (trecord != NULL)
+       { 
+        Target[l][m++]= atof(trecord);
+        trecord = strtok(NULL,",");       
+       }
+       ++l ;
+       ++x ; 
+     }
+
+
+	for (i=0;i<NUMPAT+1;i++)
+	{
+		for (j=0;j<NUMIN+1;j++)
+		{
+			printf ("Input[%d][%d] = %f\n",i,j,Input[i][j]);
+		}
+		printf ("\n");
+	}
+
+        printf ("\n");
+        printf ("\n");
+
+	for (i=0;i<NUMPAT+1;i++)
+	{
+		for (j=0;j<NUMOUT+1;j++)
+		{
+			printf ("Target[%d][%d] = %f\n",i,j,Target[i][j]);
+		}
+		printf ("\n");
+	}
+
+    fclose(tstream);
+    fclose(fstream);
     double SumH[NUMPAT+1][NUMHID+1], WeightIH[NUMIN+1][NUMHID+1], Hidden[NUMPAT+1][NUMHID+1];
     double SumO[NUMPAT+1][NUMOUT+1], WeightHO[NUMHID+1][NUMOUT+1], Output[NUMPAT+1][NUMOUT+1];
     double DeltaO[NUMOUT+1], SumDOW[NUMHID+1], DeltaH[NUMHID+1];
     double DeltaWeightIH[NUMIN+1][NUMHID+1], DeltaWeightHO[NUMHID+1][NUMOUT+1];
-    double Error, eta = 0.5, alpha = 0.9, smallwt = 0.5;
-
-    for( j = 1 ; j <= NumHidden ; j++ ) {    /* initialize WeightIH and DeltaWeightIH */
+    double Error, eta = 0.1, alpha = 0.4, smallwt = 0.5;
+    
+     for( j = 1 ; j <= NumHidden ; j++ ) {    /* initialize WeightIH and DeltaWeightIH */
         for( i = 0 ; i <= NumInput ; i++ ) {
             DeltaWeightIH[i][j] = 0.0 ;
             WeightIH[i][j] = 2.0 * ( rando() - 0.5 ) * smallwt ;
@@ -38,7 +104,6 @@ int main() {
 
     for( epoch = 0 ; epoch < 100000 ; epoch++) {    /* iterate weight updates */
  
-        printf("Entered epoch=%d\n",epoch);
         for( p = 1 ; p <= NumPattern ; p++ ) {    /* randomize order of individuals */
             ranpat[p] = p ;
         }
@@ -47,8 +112,6 @@ int main() {
             int k=rando();
             //printf("rando()=%d\n",k);
             np = p + k * ( NumPattern + 1 - p ) ;
-
-              printf("np=%d   ranpat[%d]=%d\n",np,np,ranpat[np]);
 
             op = ranpat[p] ; ranpat[p] = ranpat[np] ; ranpat[np] = op ;
         }
@@ -74,7 +137,7 @@ int main() {
 /*              Output[p][k] = SumO[p][k];      Linear Outputs */
                 
                 Error += 0.5 * (Target[p][k] - Output[p][k]) * (Target[p][k] - Output[p][k]) ;   /* SSE i.e. stochastic series expansion*/
-/*              Error -= ( Target[p][k] * log( Output[p][k] ) + ( 1.0 - Target[p][k] ) * log( 1.0 - Output[p][k] ) ) ;    Cross-Entropy Error */
+/*             Error -= ( Target[p][k] * log( Output[p][k] ) + ( 1.0 - Target[p][k] ) * log( 1.0 - Output[p][k] ) ) ;    Cross-Entropy Error */
                 DeltaO[k] = (Target[p][k] - Output[p][k]) * Output[p][k] * (1.0 - Output[p][k]) ;   /* Sigmoidal Outputs, SSE */
 /*              DeltaO[k] = Target[p][k] - Output[p][k];     Sigmoidal Outputs, Cross-Entropy Error */
 /*              DeltaO[k] = Target[p][k] - Output[p][k];     Linear Outputs, SSE */
@@ -103,25 +166,24 @@ int main() {
                 }
             }
         }
-
-        printf("\nEpoch %-5d :   Error = %f\n", epoch, Error) ; 
+ 
 
         if( epoch%100 == 0 ) fprintf(stdout, "\nEpoch %-5d :   Error = %f\n", epoch, Error) ;
         if( Error < 0.0004 ) break ;  /* stop learning when 'near enough' */
     }
 
     fprintf(stdout, "\n\nNETWORK DATA - EPOCH %d\n\nPat\t", epoch) ;   /* print network outputs */
-    for( i = 1 ; i <= NumInput ; i++ ) {
+/*    for( i = 1 ; i <= NumInput ; i++ ) {
         fprintf(stdout, "Input%-4d\t", i) ;
-    }
+    } */
     for( k = 1 ; k <= NumOutput ; k++ ) {
         fprintf(stdout, "Target%-4d\tOutput%-4d\t", k, k) ;
     }
     for( p = 1 ; p <= NumPattern ; p++ ) {
     fprintf(stdout, "\n%d\t", p) ;
-        for( i = 1 ; i <= NumInput ; i++ ) {
+      /*  for( i = 1 ; i <= NumInput ; i++ ) {
             fprintf(stdout, "%f\t", Input[p][i]) ;
-        }
+        }*/
         for( k = 1 ; k <= NumOutput ; k++ ) {
             fprintf(stdout, "%f\t%f\t", Target[p][k], Output[p][k]) ;
         }
